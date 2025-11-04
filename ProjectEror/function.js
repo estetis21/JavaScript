3a63e9ebfd75d1844ab9febacf081beb7f373f4b
estetis21 <edridze.steam@gmail.com>
2025-11-04 21:58:10 +0400

    Add files via upload

---
+++ ProjectEror/function.js
@@ -0,0 +1,19 @@
+/*function chek(){
+    if(!event.target.classList.contains('but')) return;
+    if(event.target.classList.contains('CA')) return;
+
+    conc.textContent = '';
+
+    const Number = event.terget.textContent;
+
+    if(Num.includes(Number)){
+        a += Number;
+        console.log(a,b,sign);
+    }
+}
+*/
+function DivineLight(){
+    a = '';
+    b = '';
+    sign = '';
+}
---
+++ ProjectEror/main.js
@@ -0,0 +1,31 @@
+/*let a = '';
+let b = '';
+let sing = '';
+
+const Num = ['0','1','2','3','4','5','6','7','8','9','0','.'];
+const oper = ['+','-','/','x'];
+
+const conc = document.querySelector('.calculator p');
+
+
+
+document.getElementById('CA').onclick = DivineLight;
+
+//buttons.addEventListener('click', chek())
+
+document.querySelector('.buttons').onclick = (event) => {
+    if(!event.target.classList.contains('but')) return;
+    if(event.target.classList.contains('CA')) return;
+
+    conc.textContent = '';
+
+    const Number = event.terget.textContent;
+
+    if(Num.includes(Number)){
+        a += Number;
+        console.log(a,b,sign);
+    }
+}
+*/
+const myel = document.getElementById('');
+myel.hidden = true;
